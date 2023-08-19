3336c6bf71db28621797c42e1435d7e3aecf562d
abhinavnimje1986 <abhinavnimje12@gmail.com>
2023-08-19 16:28:33 +0530

    Program Commit

---
+++ First.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# First script to run shell script.
+# We are going to tun Python and Java programs. 
+
+echo "Hello $1"
+echo "Initiating script execution now ..."
+java Hello.java
+python Hello.py
+echo "Execution successful !!!"
---
+++ Hello.py
@@ -0,0 +1,1 @@
+print "Hello Abhinav. Python is installed"
